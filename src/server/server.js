6f72f897347be60d7611e46760f0d06401887cd0
Alinur Ashke <forever.alinur@gmail.com>
2022-01-17 14:20:34 +0600

    feat: update client-id

--- src/server/server.js
+++ src/server/server.js
@@ -7,6 +7,8 @@ import axios from 'axios';
 
 const PORT = process.env.PORT || 3000;
 
+export const HOST = process.env.HOST || 'localhost:3000'
+
 const app = express();
 
 app.use('/static', express.static('./dist/client'));
@@ -15,9 +17,9 @@ app.use('/static', express.static('./dist/client'));
 app.get('/auth', (req, res) => {
   axios.post(
     'https://www.reddit.com/api/v1/access_token', 
-    `grant_type=authorization_code&code=${req.query.code}&redirect_uri=http://localhost:3000/auth`,
+    `grant_type=authorization_code&code=${req.query.code}&redirect_uri=http://${HOST}/auth`,
     { 
-      auth: { username: process.env.CLIENT_ID, password: 'EmuC04f1ckYXf7OLCPwGUTL89dl4bw'},
+      auth: { username: process.env.CLIENT_ID, password: process.env.SECRET},
       headers: { 'Content-type': 'application/x-www-form-urlencoded' }
     }
     )
