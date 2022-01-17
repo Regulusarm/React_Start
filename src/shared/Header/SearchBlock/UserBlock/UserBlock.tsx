ce4d4e422cd46862e58b703e9745e7288d7a497d
Alinur Ashke <forever.alinur@gmail.com>
2022-01-17 15:55:46 +0600

    feat: update client-id

--- src/shared/Header/SearchBlock/UserBlock/UserBlock.tsx
+++ src/shared/Header/SearchBlock/UserBlock/UserBlock.tsx
@@ -3,6 +3,7 @@ import { Break } from '../../../Break';
 import { Text, EColors } from '../../../Text/Text'
 import { IconAnon } from '../../../Icons/Icon/IconAnon';
 import styles from './userblock.scss';
+import { HOST } from '../../../../server/server'
 
 interface IUserBlockProps {
   avatarSrc?: string;
@@ -13,7 +14,7 @@ interface IUserBlockProps {
 export function UserBlock({ avatarSrc, userName, loading }: IUserBlockProps) {
   return (
    <a 
-   href={`https://www.reddit.com/api/v1/authorize?client_id=wPuZkCZI9S4RJ87ZKWwc0w&response_type=code&state=random_string&redirect_uri=https://re-mirror.herokuapp.com/auth&duration=permanent&scope=read submit identity`}
+   href={`https://www.reddit.com/api/v1/authorize?client_id=wPuZkCZI9S4RJ87ZKWwc0w&response_type=code&state=random_string&redirect_uri=${HOST}/auth&duration=permanent&scope=read submit identity`}
    className={styles.userBox}
    >
     <div className={styles.avatarBox}>
